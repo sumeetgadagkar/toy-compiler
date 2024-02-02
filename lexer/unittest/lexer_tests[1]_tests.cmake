c21cbea8b218fb79efbf94cee7aeadb7b2db71c6
Sumeet Gadagkar <zoro.ppz.g@gmail.com>
2024-02-02 15:06:44 -0500

    update gitognore

--- lexer/unittest/lexer_tests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( HelloTest.BasicAssertions /home/sumeet/Documents/projects/toy-compiler/lexer/unittest/lexer_tests [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties( HelloTest.BasicAssertions PROPERTIES WORKING_DIRECTORY /home/sumeet/Documents/projects/toy-compiler/lexer/unittest SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( lexer_tests_TESTS HelloTest.BasicAssertions)
