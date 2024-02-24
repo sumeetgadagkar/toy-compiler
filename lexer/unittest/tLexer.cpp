de7596db17314476d63a756fe345b1d434fc10f6
Sumeet Gadagkar <zoro.ppz.g@gmail.com>
2024-02-24 13:54:35 -0500

    random

--- lexer/unittest/tLexer.cpp
+++ lexer/unittest/tLexer.cpp
@@ -1,7 +1,20 @@
+#include "lexer/include/Lexer.hpp"
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+namespace {}
+
+TEST(Lexer, Test1) {
+
+  std::stringstream code(R"(
+                         def main() {
+                          var a = [1, 2, 3];
+                          print(a);
+                         }
+                         )");
+
+  toy::lexer::Lexer lex(std::move(code));
+  std::cout << lex.getNextToken() << " : " << lex.getLiteral() << std::endl;
+
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
