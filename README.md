8f6bed1f5f3d9f01db23c33aa901042538bd625b
Sumeet Gadagkar <zoro.ppz.g@gmail.com>
2024-03-07 16:27:26 -0500

    updated readme and make files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # toy-compiler
-This is an implementation of a toy compiler using flex, bison, MLIR &amp; LLVM. Used mainly for learning optimizations.
+
+This is an implementation of a toy compiler using MLIR. Used mainly for learning optimizations.
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "lexer/include/Lexer.hpp"
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
   std::stringstream code("return");
   toy::lexer::Lexer lex(std::move(code));
   std::cout << lex.getNextToken() << " : " << lex.getLiteral() << std::endl;
